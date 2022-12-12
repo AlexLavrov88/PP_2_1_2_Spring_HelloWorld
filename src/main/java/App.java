8b96e2552ca9a8e74df7bd61287320886bba1741
AlexLavrov88 <lavrov_alex@list.ru>
2022-12-12 15:17:36 +0300

    Готовое решение  /rev 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,20 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
-        System.out.println("test");
+        System.out.println(cat.getSay());
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
     }
 }
